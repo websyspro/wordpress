a86ce826e00a3a026ecc51c22f80199961384d84
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-09-04 15:13:19 -0300

    Release 1.0.3

---
+++ devTools.php
@@ -0,0 +1,15 @@
+<?php
+
+use Websyspro\DevTools\Interfaces\DevTools;
+
+require "./vendor/autoload.php";
+return new DevTools(
+  includes: [],
+  excludes: [],
+  webSocketHost: "0.0.0.0",
+  webSocketPort: 3002,
+  httpServerPort: 3001,
+  documentRoot: "vendor/websyspro/wpengine/src/Core",
+  errorReporting: "",
+  scriptName: ""
+);
