c1722f652ff7dbc100dc7a3e2bab29edce598f92
Websyspro <cpd.emersontsa@gmail.com>
2026-09-25 17:19:57 -0300

    Release 1.0.12

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 
-declare(strict_types=1);
-
-define( "ROUTE_ROOT", __DIR__ );
-define( "PUBLICS", [
-  __DIR__ . "/vendor/websyspro/wpengine/src/Core",
-  __DIR__ . "/src",
-]);
-
-require __DIR__ . "/vendor/autoload.php";
-require __DIR__ . "/vendor/websyspro/devtools/router.php";
+// Silence is golden.
